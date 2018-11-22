2f789409ee8448ff6a5f2f1191f1807a98d6bd29
Carlos K <carloskotachoamd@hotmail.com>
2018-11-22 02:14:39 -0200

    Commit inicial

---
+++ src/main/java/br/edu/utfpr/math/MathematicsSpringApplication.java
@@ -0,0 +1,12 @@
+package br.edu.utfpr.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathematicsSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MathematicsSpringApplication.class, args);
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+/**
+ * Author:  Carlos Henrique
+ * Created: Nov 21, 2018
+ */
+INSERT INTO user (id, email, name, password) VALUES ('1', 'admin@hotmail.com', 'Admin', 'admin123');
+INSERT INTO user (id, email, name, password) VALUES ('2', 'user@hotmail.com', 'User', 'user123');
+
