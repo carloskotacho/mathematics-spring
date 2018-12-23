1cddb4ed285b8b1f6b030eb72718772554eca57d
root <carloskotachoamd@hotmail.com>
2018-12-23 18:09:49 -0200

    Add: message notations

--- src/main/java/br/edu/utfpr/math/dto/UserDTO.java
+++ src/main/java/br/edu/utfpr/math/dto/UserDTO.java
@@ -2,6 +2,8 @@ package br.edu.utfpr.math.dto;
 
 import br.edu.utfpr.math.model.Role;
 import java.util.List;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 
 /**
@@ -11,8 +13,12 @@ import lombok.Getter;
 @Getter
 public class UserDTO {
     
+    @NotBlank(message = "{name.not.blank}")
     private String name;
+    @NotBlank(message = "{email.not.blank}")
+    @Email(message = "{email.not.valid}")
     private String email;
+    @NotBlank(message = "{password.not.blank}")
     private String password;
     
     /*
